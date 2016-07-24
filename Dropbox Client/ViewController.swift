b3c56f206f36fd4183b1643cc1a7148d9beecd0f
Luke Stringer <luke@lukestringer.me>
2016-07-24 13:45:50 +0100

    Upload a picked image

--- Dropbox Client/ViewController.swift
+++ Dropbox Client/ViewController.swift
@@ -8,33 +8,91 @@
 
 import UIKit
 import SwiftyDropbox
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let dateFormatter = NSDateFormatter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Format used to generate file name, which should not contain / chars
+        dateFormatter.dateFormat = "dd-MM-yyyy, hh mm ss"
     }
 
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        guard let client = Dropbox.authorizedClient else {
+        guard Dropbox.authorizedClient != nil else {
             Dropbox.authorizeFromController(self)
             return
         }
         
-        // Get the current user's account info
-        client.users.getCurrentAccount().response { response, error in
-            print("*** Get current account ***")
-            if let account = response {
-                print("Hello \(account.name.givenName)!")
-            } else {
-                print(error!)
-            }
-        }
-
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .PhotoLibrary
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func uploadNameForImageAtReferenceURL(referenceURL: NSURL) -> String {
+        let result = PHAsset.fetchAssetsWithALAssetURLs([referenceURL], options: nil)
+        let asset = result.firstObject as! PHAsset
+        return "/Photo \(dateFormatter.stringFromDate(asset.creationDate!)).png"
     }
 
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        print("Cancelled image pick")
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        picker.dismissViewControllerAnimated(true) {
+            print(info)
+            
+            guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+                print("Failed to get picked image")
+                return
+            }
+            
+            guard let data = UIImagePNGRepresentation(image) else {
+                print("Cannot make data")
+                return
+            }
+            
+            
+            let imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+            let filename = self.uploadNameForImageAtReferenceURL(imageURL)
+            
+            let client = Dropbox.authorizedClient!
+            client.files.upload(path: filename, input: data).response({ (uploadResponse, uploadError) in
+                if let uploadName = uploadResponse?.name, uploadRevision = uploadResponse?.rev {
+                    print("*** Upload file ****")
+                    print("Uploaded file name: \(uploadName)")
+                    print("Uploaded file revision: \(uploadRevision)")
+                    
+                    client.files.getMetadata(path: filename).response({ (response, metaDataError) in
+                        if let metaData = response {
+                            if let file = metaData as? Files.FileMetadata {
+                                print("This is a file with path: \(file.pathLower)")
+                                print("File size: \(file.size)")
+                            } else if let folder = metaData as? Files.FolderMetadata {
+                                print("This is a folder with path: \(folder.pathLower)")
+                            }
+                        }
+                        else {
+                            print(metaDataError!)
+                        }
+                    })
+                }
+                else {
+                    print(uploadError!)
+                }
+            })
+            
+        }
+    }
 }
 
